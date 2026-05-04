0f6f7d480f8217b7eb17daaec28d3af07009c15f
Robin Malfait <malfait.robin@gmail.com>
2026-05-04 16:44:28 +0200

    Ensure `--value(…)` is required in functional `@utility` definitions (#20005)
    While working on #19989, I noticed that `--value(…)` inside functional
    `@utility` definitions is not required right now.
    
    That means that the following CSS is valid:
    ```css
    @utility foo-* {
      color: red;
    }
    ```
    
    But this doesn't really makes sense, because this now accepts a value
    and `foo-a`, `foo-b` and `foo-c` would generate the following CSS:
    ```css
    .foo-a {
      color: red;
    }
    .foo-b {
      color: red;
    }
    .foo-c {
      color: red;
    }
    ```
    The `a`, `b`, and `c` are not doing anything here apart from making your
    CSS bigger. So this is very likely an actual bug that you forgot to use
    `--value(…)`.
    
    Additionally, if a `--value(…)` was used, but it didn't resolve
    anything, then we already properly discared the candidate.
    
    ## Test plan
    
    1. Add test to ensure `--value(…)` is required in functional `@utility`
    definitions
    2. Existing tests pass

--- packages/tailwindcss/src/utilities.test.ts
+++ packages/tailwindcss/src/utilities.test.ts
@@ -28870,6 +28870,40 @@ describe('custom utilities', () => {
   })
 
   describe('functional utilities', () => {
+    test('functional utilities require a `--value(…)`', async () => {
+      let input = css`
+        @utility tab-* {
+          tab-size: 4;
+        }
+
+        @tailwind utilities;
+      `
+
+      expect(await compileCss(input, ['tab', 'tab-foo'])).toEqual('')
+    })
+
+    test('functional utilities must resolve at least one `--value(…)`', async () => {
+      let input = css`
+        @utility tab-* {
+          tab-size: --value(integer);
+        }
+
+        @tailwind utilities;
+      `
+
+      expect(await compileCss(input, ['tab-1', 'tab-2'])).toMatchInlineSnapshot(`
+        ".tab-1 {
+          tab-size: 1;
+        }
+
+        .tab-2 {
+          tab-size: 2;
+        }"
+      `)
+
+      expect(await compileCss(input, ['tab', 'tab-foo', 'tab-2.5'])).toEqual('')
+    })
+
     test('resolving values from `@theme`', async () => {
       let input = css`
         @theme reference {
